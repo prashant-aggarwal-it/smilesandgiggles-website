1d0e78bb58698e0a7e8551b21513dbcf3f1dde8b
Prashant Aggarwal <prashant.aggarwal.it@gmail.com>
2026-09-18 13:29:21 +0530

    form issue fixed

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -32,7 +32,7 @@ export function ContactForm({ submitted = false }: { submitted?: boolean }) {
         if (typeof value === "string") encoded.append(key, value);
       });
 
-      const response = await fetch("/", {
+      const response = await fetch("/netlify-forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encoded.toString(),
--- src/components/get-involved/GetInvolvedInterestForm.tsx
+++ src/components/get-involved/GetInvolvedInterestForm.tsx
@@ -28,7 +28,7 @@ export function GetInvolvedInterestForm({ submitted = false }: { submitted?: boo
         if (typeof value === "string") encoded.append(key, value);
       });
 
-      const response = await fetch("/", {
+      const response = await fetch("/netlify-forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encoded.toString(),
